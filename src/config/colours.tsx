20f4ee7c8ab6987300d02622bda5c0c27ce02306
Moya <73916478+MoyaSparklefarts@users.noreply.github.com>
2022-09-08 17:36:53 +1200

    Update colours.tsx

--- src/config/colours.tsx
+++ src/config/colours.tsx
@@ -17,17 +17,17 @@ export const about_background = '#FBFFFC'
 export const about_text = '#2B2D3F'
 
 // Roadmap section
-export const roadmap_background = '#ed962c'
+export const roadmap_background = '#F4DE4D'
 export const roadmap_text = '#2B2D3F'
 
 // Team section
-export const team_background = '#3c687d'
+export const team_background = '#8C1528'
 export const team_text = '#FBFFFC'
 export const team_member_background = '#FBFFFC'
 export const team_member_text = '#2B2D3F'
 
 // FAQ section
-export const faq_background = '#ed962c'
+export const faq_background = '#F4DE4D'
 export const faq_title_text = '#2B2D3F'
 export const faq_question_background = '#FBFFFC'
 export const faq_question_text = '#2B2D3F'
@@ -45,4 +45,4 @@ export const text_shadow = '3px 3px 5px #00000044'
 export const box_shadow = '0 0 10px 5px #00000022'
 
 // Social
-export const twitter_blue = '#1da1f2'
+export const twitter_blue = '#8C1528'
